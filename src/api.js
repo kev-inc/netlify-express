3a4a5642cdd23682795c180f088dc605d2c136d5
kev-inc <kevinchanxy@gmail.com>
2020-09-07 09:12:00 +0000

    added lowdb

--- src/api.js
+++ src/api.js
@@ -1,14 +1,19 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
+const adapter = new FileSync('db.json')
+const db = low(adapter)
 
 const app = express()
-
 const router = express.Router()
 
+db.defaults({ todos: [{id: 1, message: "Do CS3219 assignment"}], count: 1 })
+  .write()
+
 router.get('/', (req, res) => {
-	res.json({
-		message: 'hello world'
-	})
+	const todos = db.get('todos').value()
+	res.json(todos)
 })
 
 app.use('/.netlify/functions/api', router)
